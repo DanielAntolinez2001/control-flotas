9be8ac64b7d1f3b8af0c1e50ccefb1a0ca8c80fd
Daniel Antolinez <daniel.antolinez0505@gmail.com>
2024-05-07 18:54:14 -0500

    feat: creation of navbar and sidebar for navigation interface

---
+++ client-project/app/(root)/Mis-Camiones/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MisCamiones = ({ params }: { params: { id: string } }) => {
+  return <div>MisCamiones: {params.id}</div>;
+};
+
+export default MisCamiones;
---
+++ client-project/constants/index.ts
@@ -0,0 +1,17 @@
+export const sidebarLinks = [
+  {
+    imgUrl: "/icons/grafico-histograma.svg",
+    route: "/",
+    label: "Dashboard",
+  },
+  {
+    imgUrl: "/icons/lado-del-camion.svg",
+    route: "/my-trucks",
+    label: "My Trucks",
+  },
+  {
+    imgUrl: "/icons/llave-inglesa.svg",
+    route: "/maintenance",
+    label: "Maintenance",
+  },
+];
