f00ad8c982b005839dcdb7e31bc61740241a7f78
Daniel Antolinez <daniel.antolinez0505@gmail.com>
2024-05-16 14:08:03 -0500

    fix: component map

--- nextadmin/app/ui/map/page.jsx
+++ nextadmin/app/ui/map/page.jsx
@@ -1,33 +1,39 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api';
+"use client";
+import React, { useState, useEffect } from "react";
+import { LoadScript, GoogleMap, Marker } from "@react-google-maps/api";
 
 const Mapa = () => {
-    const [mapa, setMapa] = useState(null);
-  
-    useEffect(() => {
-      if (typeof window !== 'undefined' && window.google) {
-        setMapa(new google.maps.Map(document.getElementById('mapa'), {
+  const [mapa, setMapa] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.google) {
+      setMapa(
+        new google.maps.Map(document.getElementById("mapa"), {
           zoom: 15,
-          center: { lat: 40.7128, lng: -74.0060 }, // Coordenadas de ejemplo
-        }));
-      }
-    }, [mapa]);
-  
-    return (
+          center: { lat: 40.7128, lng: -74.006 }, // Coordenadas de ejemplo
+        })
+      );
+    }
+  }, [mapa]);
+
+  return (
+    <div>
       <LoadScript
         googleMapsApiKey="{AIzaSyAmEylscEWoNMXNKyK3q5s3zTw341YbNWM}"
-        libraries={['places']}
+        libraries={["places"]}
         onLoad={() => {
-          setMapa(new google.maps.Map(document.getElementById('mapa'), {
-            zoom: 15,
-            center: { lat: 40.7128, lng: -74.0060 }, // Coordenadas de ejemplo
-          }));
+          setMapa(
+            new google.maps.Map(document.getElementById("mapa"), {
+              zoom: 15,
+              center: { lat: 40.7128, lng: -74.006 }, // Coordenadas de ejemplo
+            })
+          );
         }}
       >
-        <div id="mapa" style={{ width: '600px', height: '400px' }} />
+        <div id="mapa" style={{ width: "600px", height: "400px" }} />
       </LoadScript>
-    );
+    </div>
+  );
 };
-  
+
 export default Mapa;
