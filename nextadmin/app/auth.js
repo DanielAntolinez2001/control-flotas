c8110c603d76663f72af67f85e2a384a2af79ebf
Jassy0205 <a.z23042016a@gmail.com>
2024-05-28 11:14:49 -0500

    fix: Login bug fix initial

--- nextadmin/app/auth.js
+++ nextadmin/app/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
 import { authConfig } from "./auth.config";
 
 import { PrismaClient } from "@prisma/client";
